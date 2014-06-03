b09475030b6d4654244b1671b93632acae6b2d1b
Joakim Beijar <jbeijar@mios.fi>
2014-06-03 14:58:17 +0300

    Fixing watches etc.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 // Run 'gulp' to do the important stuff
 var  gulp = require('gulp')
-	,prefixer = require('gulp-autoprefixer')
-	,less = require('gulp-less')
-	,path = require('path')
-  ,concat = require('gulp-concat')
-	,rename = require('gulp-rename')
-	,connect = require('gulp-connect');
+    ,watch = require('gulp-watch')
+    ,prefixer = require('gulp-autoprefixer')
+    ,less = require('gulp-less')
+    ,path = require('path')
+    ,concat = require('gulp-concat')
+    ,rename = require('gulp-rename')
+    ,connect = require('gulp-connect');
 
 gulp.task('connect', function(){
   connect.server({
@@ -14,41 +15,39 @@ gulp.task('connect', function(){
   })
 });
 
-gulp.task('less', function () {
-  gulp
-    .src('./all.less')
-    .pipe(
-      less({
-        rootPath: 'style',
-        paths: ['less'],
-        sourceMap: true ,
-        relativeUrls: true,
-        x: true
-      }).on("error", function(err) {
-        console.error(err.message);
-      })
-    )
-    .pipe(prefixer('last 2 versions', 'ie 9'))
+gulp.task('less', function() {
+  return gulp
+    .src('./less/all.less')
+    .pipe(less({
+      rootPath: 'style',
+      paths: ['less'],
+      sourceMap: true ,
+      relativeUrls: true,
+      x: true
+    }).on("error", function(err) {
+      console.error(err.message);
+    }))
     .pipe(gulp.dest('./app/style'))
     .pipe(connect.reload())
 });
 
 gulp.task('scripts', function() {
   return gulp
-    .src(['./script/**/*.js'])
+    .src(['./scripts/**/*.js'])
     .pipe(concat('all.js'))
-    .pipe(gulp.dest('./app'))
+    .pipe(gulp.dest('./app/scripts'))
     .pipe(connect.reload())
 });
 
 gulp.task('html', function(){
-  gulp.src('app/*.html')
+  return gulp.src('./app/**/*.html')
     .pipe(connect.reload());
 });
 
-gulp.task('default', ['less','scripts','watch','connect']);
 gulp.task('watch', function() {
-  gulp.watch('./less/**/*.less', ['less']);
-  gulp.watch('./scripts/**/*.js', ['scripts']);
-  gulp.watch('./app/**/*.html', ['html']);
+  watch({ glob: 'less/**/*.less'  }, ['less']   );
+  watch({ glob: 'scripts/**/*.js' }, ['scripts']);
+  watch({ glob: 'app/**/*.html'   }, ['html']   );
 });
+
+gulp.task('default', ['less', 'scripts', 'watch', 'connect']);
